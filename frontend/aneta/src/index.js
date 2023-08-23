55a7c2374fde0cb2518466d59ab5b05a85ba9f12
heathhartley <heathhartley55@gmail.com>
2023-08-22 21:24:33 -0600

    login

--- frontend/aneta/src/index.js
+++ frontend/aneta/src/index.js
@@ -5,11 +5,15 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 // import App from "./components/App"
 import 'bootstrap/dist/css/bootstrap.min.css';   
+import { AuthProvider } from './context/AuthProvider'; v
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+    
   </React.StrictMode>
 );
 
